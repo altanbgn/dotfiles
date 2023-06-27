1c183f097b47e9e736d478d49f7c88ded854dcc9
Altanbagana <altanbagana.e@gobicashmere.com>
2023-06-27 15:05:27 +0800

    changed shortcut

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -4,7 +4,7 @@ require('telescope').load_extension('env')
 
 vim.keymap.set("n", "<leader>ph", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<leader>ps", function()
+vim.keymap.set("n", "<leader>pg", function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
