71e83c9eb5ee503b336c9bbab464ac6fb0dc484f
altanbgn <altanbagana@protonmail.com>
2024-08-13 16:36:21 +0800

    started using C-o so i need my C-i back xd

--- nvim/.config/nvim/plugin/harpoon.lua
+++ nvim/.config/nvim/plugin/harpoon.lua
@@ -4,11 +4,5 @@ harpoon:setup({})
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-vim.keymap.set("n", "<C-m>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-e>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-i>", function() harpoon:list():select(4) end)
-
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():previous() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-e>", function() harpoon:list():next() end)
