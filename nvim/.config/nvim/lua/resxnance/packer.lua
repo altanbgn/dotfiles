05848c917106ab3777bca3c125d391a5312ba192
altanbgn <altanbagana@protonmail.com>
2024-08-24 12:01:17 +0800

    added zen mode

--- nvim/.config/nvim/lua/resxnance/packer.lua
+++ nvim/.config/nvim/lua/resxnance/packer.lua
@@ -4,94 +4,97 @@
 vim.cmd.packadd("packer.nvim")
 
 return require("packer").startup(function(use)
-  -- Packer can manage itself
-  use("wbthomason/packer.nvim")
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
 
-  -- Dependant
-  use("nvim-lua/plenary.nvim")
+    -- Dependant
+    use("nvim-lua/plenary.nvim")
 
-  -- Startup
-  use("goolord/alpha-nvim")
+    -- Startup
+    use("goolord/alpha-nvim")
 
-  use("mbbill/undotree")
-  use("nvim-treesitter/nvim-treesitter-context")
-  use("eandrju/cellular-automaton.nvim")
-  use("nvim-lualine/lualine.nvim")
+    use("mbbill/undotree")
+    use("nvim-treesitter/nvim-treesitter-context")
+    use("eandrju/cellular-automaton.nvim")
+    use("nvim-lualine/lualine.nvim")
 
-  -- Org
-  use({
-    'nvim-orgmode/orgmode',
-    config = function()
-      -- Setup orgmode
-      require('orgmode').setup({
-        org_agenda_files = '~/Personal/.orgfiles/**/*',
-        org_default_notes_file = '~/Personal/.orgfiles/refile.org',
-      })
-    end,
-  })
+    -- Org
+    use({
+        'nvim-orgmode/orgmode',
+        config = function()
+            -- Setup orgmode
+            require('orgmode').setup({
+                org_agenda_files = '~/Personal/.orgfiles/**/*',
+                org_default_notes_file = '~/Personal/.orgfiles/refile.org',
+            })
+        end,
+    })
 
-  -- AI
-  -- use("github/copilot.vim")
-  use("Exafunction/codeium.vim")
+    -- AI
+    -- use("github/copilot.vim")
+    use("Exafunction/codeium.vim")
 
-  -- Indent lines and indicators
-  use({
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {}
-  })
+    -- Indent lines and indicators
+    use({
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {}
+    })
 
-  -- Colorscheme
-  use({ "rose-pine/neovim", as = "rose-pine" })
+    -- Colorscheme
+    use({ "rose-pine/neovim", as = "rose-pine" })
 
-  -- Git
-  use("tpope/vim-fugitive")
-  use("lewis6991/gitsigns.nvim")
+    -- Git
+    use("tpope/vim-fugitive")
+    use("lewis6991/gitsigns.nvim")
 
-  -- Utility
-  use({
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-  })
-  use("nvim-tree/nvim-web-devicons")
-  use("folke/trouble.nvim")
-  use("numToStr/Comment.nvim")
-  use({
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.2"
-  })
+    -- Zen mode
+    use("folke/zen-mode.nvim")
 
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = function()
-      require("nvim-treesitter.install").update({ with_sync = true })
-    end
-  })
+    -- Utility
+    use({
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+    })
+    use("nvim-tree/nvim-web-devicons")
+    use("folke/trouble.nvim")
+    use("numToStr/Comment.nvim")
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.2"
+    })
 
-  use({
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v1.x",
-    requires = {
-      -- LSP Support
-      { "williamboman/mason.nvim" },
-      { "williamboman/mason-lspconfig.nvim" },
-      { "neovim/nvim-lspconfig" },
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            require("nvim-treesitter.install").update({ with_sync = true })
+        end
+    })
 
-      -- Autocompletion
-      {
-        "hrsh7th/nvim-cmp",
-        branch = "main",
-        commit = "7e348da6e5085ac447144a2ef4b637220ba27209"
-      },
-      { "hrsh7th/cmp-buffer" },
-      { "hrsh7th/cmp-path" },
-      { "saadparwaiz1/cmp_luasnip" },
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-nvim-lua" },
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v1.x",
+        requires = {
+            -- LSP Support
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+            { "neovim/nvim-lspconfig" },
 
-      -- Snippets
-      { "L3MON4D3/LuaSnip" },
-      { "rafamadriz/friendly-snippets" },
-    }
-  })
+            -- Autocompletion
+            {
+                "hrsh7th/nvim-cmp",
+                branch = "main",
+                commit = "7e348da6e5085ac447144a2ef4b637220ba27209"
+            },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
+
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+        }
+    })
 end)
