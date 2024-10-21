2367d059fdb0a4852b379a8de1f1c1c68d416eec
altanbgn <altanbagana@protonmail.com>
2024-10-21 14:14:52 +0800

    chore: hehehe

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -25,7 +25,6 @@ return {
         cmd = "Trouble",
         opts = {},
         keys = {
-
             { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",                        desc = "Diagnostics (Trouble)" },
             { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",           desc = "Buffer Diagnostics (Trouble)" },
             { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>",                desc = "Symbols (Trouble)" },
